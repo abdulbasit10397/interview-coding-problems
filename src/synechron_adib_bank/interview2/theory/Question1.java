5689d0c1591124caaa0a4520c4e6752d12f7d392
Abdul Basit <abdulbasit10397@gmail.com>
2023-12-09 11:54:29 +0500

    INTERVIEW-07 Synechron ADIB part 2 - Coding Question

---
+++ src/synechron_adib_bank/interview2/theory/Question1.java
@@ -0,0 +1,17 @@
+package synechron_adib_bank.interview2.theory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Question1 {
+    public static void main (String[] args) {
+        List<Integer> numbers = Arrays.asList(21, 2, 3, 24, 5);
+
+        List<Integer> numbersThatStartWith2 = numbers.stream()
+                .filter(n1 -> String.valueOf(n1).charAt(0)=='2')
+                .collect(Collectors.toList());
+
+        System.out.println(numbersThatStartWith2);
+    }
+}
