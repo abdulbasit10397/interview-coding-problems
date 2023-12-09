13f4182845adc1a99001cfbc3de8cff933b6e46c
Abdul Basit <abdulbasit10397@gmail.com>
2023-12-09 12:01:15 +0500

    INTERVIEW-07 Synechron ADIB part 2 - Added coding question description

--- src/synechron_adib_bank/interview2/theory/Question1.java
+++ src/synechron_adib_bank/interview2/theory/Question1.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Question: Write a java 8 code to find numbers starting with 2 from a list of numbers
+ */
 public class Question1 {
     public static void main (String[] args) {
         List<Integer> numbers = Arrays.asList(21, 2, 3, 24, 5);
